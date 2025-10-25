cdd2a93ec5e8659c560f20232ca7b8e0d71ece52
taguchi kazuki <elfubolfelicidadsuprema02@gmail.com>
2025-10-26 01:18:35 +0900

    add: buildtag 面白いね

---
+++ 20250927/workshop/buildtag/skeleton/step1/util.go
@@ -0,0 +1,15 @@
+//go:build go1.25
+
+//TODO: go1.25以上を対象としたビルドタグを記述する
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func init() {
+	msg := fmt.Sprintf("panic in %s", runtime.Version())
+	panic(msg)
+}
---
+++ 20250927/workshop/buildtag/skeleton/step2/util.go
@@ -0,0 +1,11 @@
+//go:build go1.25
+
+package main
+
+import "fmt"
+
+func init() {
+	// TODO: go vetが検出するように%dを%sに変える
+	// fmt.Printf("wrong format %d", 100)
+	fmt.Printf("wrong format %s", "100")
+}
