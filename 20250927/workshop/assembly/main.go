62359f84451989066db8f298e029324a393e8643
taguchi kazuki <elfubolfelicidadsuprema02@gmail.com>
2025-10-26 00:05:39 +0900

    add: assembly むずい

---
+++ 20250927/workshop/assembly/main.go
@@ -0,0 +1,18 @@
+package main
+
+// なぜGoアセンブリを学ぶのか
+// 標準ライブラリの理解。runtime, syncなどでは頻出する。特にgoroutineをはじめとしたGoランタイムの実装ではクロスプラットフォームなアセンブリが必要。
+// Goの内部動作の理解。コンパイラがどのようなコードを生成するかを学ぶ
+
+// step1で make addする
+
+// step2 アセンブルを書いてみる
+
+// 実施的な応用
+// 標準ライブラリの理解に役立つ。
+// Go schedulerの実装理解
+// math, syncのようなランタイムレベルでの操作を必要とするライブラリの理解
+
+// 参考
+// A Quick Guide to Go's Assembler - Go 公式のアセンブリガイド
+// https://go.dev/doc/asm
---
+++ 20250927/workshop/assembly/step2/main.go
@@ -0,0 +1,10 @@
+package main
+
+// 関数の構造
+// (SB): Static Base からの相対
+// NOSPLIT: スタック拡張チェックをスキップ
+// $0-24: スタックサイズ-引数と戻り値のサイズ
+
+// TEXT ·FuncName(SB), NOSPLIT, $0-24
+//     // 関数の実装
+//     RET
